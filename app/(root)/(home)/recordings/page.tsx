1bef8201176e326f9aa5d813f8033213212bed27
Mayank-Radadiya <mayankradadiya2@gmail.com>
2024-12-26 10:01:34 +0530

    Build Error Fixed

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,9 +1,7 @@
 import CallList from "@/components/MeetingComponents/CallList";
 import { FC } from "react";
 
-interface pageProps {}
-
-const Page: FC<pageProps> = ({}) => {
+const Page: FC = ({}) => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <h1 className="text-3xl font-bold">Recording</h1>
